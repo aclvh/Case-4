f7405f212c216e73c4e99f5213daa4cb464fc467
aclvh <127091332+aclvh@users.noreply.github.com>
2023-04-06 18:41:25 +0200

    Add files via upload

--- case4.py
+++ case4.py
@@ -127,7 +127,7 @@ def grafieken():
     jaren = ('2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010',
             '2011', '2012', '2013', '2014', '2015')
     
-    col1, col2 = st.columns(2)
+    col1, col2 = st.columns([1, 4])
     
     with col1:
         # Add the choicemenu to the first column
