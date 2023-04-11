794169ded5c44cbf2b87d463bb795b3daa22d1ba
aclvh <127091332+aclvh@users.noreply.github.com>
2023-04-11 10:07:35 +0200

    Add files via upload

--- case4.py
+++ case4.py
@@ -224,7 +224,7 @@ def grafieken():
                                xaxis_title = 'Regio',
                                yaxis_title = 'Levensverwachting (in jaren)',
                                yaxis_range = [25, 100],
-                               width = 500)
+                               width = 650)
 
         fig_jaar
 
@@ -246,7 +246,7 @@ def grafieken():
                                xaxis_title = 'Regio',
                                yaxis_title = 'Levensverwachting (in jaren)',
                                yaxis_range = [25, 100],
-                               width = 600)
+                               width = 650)
         
         fig_jaar
 
